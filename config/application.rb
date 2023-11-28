7f0952c617d453a173e3299a2934ec21069c9070
TomasBarnas93 <btomek506@gmail.com>
2023-11-28 08:35:35 +0100

    2

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module DagXBackend
     # Add the following block to configure rack-cors
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'dagx.se', 'api.dagx.se', 'https://dagx.se', 'http://dagx.se'
+        origins 'dagx.se', 'api.dagx.se', 'https://dagx.se', 'http://dagx.se', 'https://dagx-backend-ec91b044f633.herokuapp.com/'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
