68081fd8994234e330c29b0de7337e87243eae97
TomasBarnas93 <btomek506@gmail.com>
2023-12-04 22:00:13 +0100

    new

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -2,15 +2,13 @@ class ContactFormController < ApplicationController
   def create
     Rails.logger.debug "Received parameters: #{contact_params.inspect}"
 
-    if params[:attachment].present? && params[:attachment].respond_to?(:read)
-      begin
-        ContactMailer.contact_email(contact_params).deliver_now
-        render json: { status: 'success' }
-      rescue => e
-        render json: { status: 'error', message: e.message }, status: :internal_server_error
-      end
-    else
-      render json: { status: 'error', message: 'Attachment is not a file or missing' }, status: :unprocessable_entity
+    begin
+      # Send email even if the attachment is not present
+      ContactMailer.contact_email(contact_params).deliver_now
+      render json: { status: 'success' }
+    rescue => e
+      Rails.logger.error "Error in processing contact form: #{e.message}"
+      render json: { status: 'error', message: e.message }, status: :internal_server_error
     end
   end
 
