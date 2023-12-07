ad479649b05a2b79e6fa34280c600641058d13cd
TomasBarnas93 <btomek506@gmail.com>
2023-12-07 19:58:55 +0100

    Test 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,14 +69,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: 'api',
-    password: ENV['MAILTRAP_PASSWORD'],  
-    address: 'smtp.mailtrap.io',
-    port: '587',
-    authentication: :cram_md5,
-    enable_starttls_auto: true
-  }
-
 end
