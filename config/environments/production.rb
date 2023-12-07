8ce9788d26889de1ebd2a28b9a601036775e22b3
TomasBarnas93 <btomek506@gmail.com>
2023-12-07 21:57:45 +0100

    Test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => 'api',
-    :password => ENV['MAILTRAP_PASSWORD'],
-    :address => 'live.smtp.mailtrap.io',
-    :host => 'live.smtp.mailtrap.io',
-    :port => '587',
-    :authentication => :cram_md5
+    user_name: 'api',
+    password: ENV['MAILTRAP_PASSWORD'],
+    address: 'live.smtp.mailtrap.io',
+    port: '587',
+    authentication: :login, 
+    enable_starttls_auto: true  
   }
   
 end
