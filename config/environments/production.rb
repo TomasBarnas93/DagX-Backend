00301de568f6ad95117f5efdaf56594c816ea63f
TomasBarnas93 <btomek506@gmail.com>
2023-12-07 08:42:13 +0100

    he

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'api',
-    password: ENV['MAILTRAP_PASSWORD'],  
+    user_name: ENV['MAILTRAP_USERNAME'],
+    password: ENV['MAILTRAP_PASSWORD'],
     address: 'smtp.mailtrap.io',
     port: '587',
     authentication: :login,
