1211175883e7c687dd97896349bba560d5fee487
TomasBarnas93 <btomek506@gmail.com>
2023-12-10 00:00:03 +0100

    Aws test

--- config/application.rb
+++ config/application.rb
@@ -26,19 +26,18 @@ module DagXBackend
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # Skip views, helpers, and assets when generating a new resource.
     config.api_only = true
-    
+
     # Add the following block to configure rack-cors
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'dagx.se', 'api.dagx.se','www.api.dagx.se', 'https://dagx.se', 'http://dagx.se',
+        origins 'dagx.se', 'api.dagx.se', 'www.api.dagx.se', 'https://dagx.se', 'http://dagx.se'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
           credentials: true
       end
     end
-    
   end
 end
