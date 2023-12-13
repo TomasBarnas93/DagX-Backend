9e8831b41f7044b13e17e71cd93accc9aa83eabd
TomasBarnas93 <btomek506@gmail.com>
2023-12-13 20:17:33 +0100

    S

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  # Allow requests from 'api.dagx.se'
-config.hosts << "api.dagx.se"
-
 end
