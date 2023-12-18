2bde179c989c64cb8e212d2ff54fbca33d17430a
TomasBarnas93 <btomek506@gmail.com>
2023-12-18 20:32:11 +0100

    Finall

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Define the root path route ("/")
+  root "welcome#index" # Adjust the controller and action as needed
 
   # Resourceful route for handling contact form submissions
   resources :contact_forms, only: [:create]
 
   get 'contact_form', to: 'contact_form#new'
   post 'contact_form', to: 'contact_form#create'
-
 end
