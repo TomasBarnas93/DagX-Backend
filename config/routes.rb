155b9e5844123c0b8eaaa7e9208e380d009908a1
TomasBarnas93 <btomek506@gmail.com>
2023-12-19 12:00:40 +0100

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-
-  # Define the root path route ("/")
-  root "welcome#index" # Adjust the controller and action as needed
+  # Set the root path to the new action of the ContactFormController
+  root "contact_form#new"
 
   # Resourceful route for handling contact form submissions
   resources :contact_forms, only: [:create]
 
+  # Explicit routes for the contact form
   get 'contact_form', to: 'contact_form#new'
   post 'contact_form', to: 'contact_form#create'
+
+  # Catch-all route to handle any unrecognized paths
+  # Redirects to the root path or you can render a custom 404 page
+  match '*path', to: redirect('/'), via: :all
 end
